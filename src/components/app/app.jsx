79c21db279c4bdac7352b8c524278d8743754003
Alex2234 <50018756+Alex2234@users.noreply.github.com>
2023-07-27 11:50:24 +0300

    Revert "Month 8/step 1"

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,59 +1,17 @@
-import React from "react";
 import styles from "./app.module.css";
-import AppHeader from "../header/header";
-import { fetchIngredients } from "../../utils/burger-api";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import IngredientsContext from "../../services/ingredientsContext";
-import SelectedIngredientsContext from "../../services/selectedIngredientsContext";
+import { data } from "../../utils/data";
 
-const App = () => {
-  const [data, setData] = React.useState([]);
-  const [error, setError] = React.useState();
-  const [selectedIngredients, setSelectedIngredients] = React.useState([]);
-
-  React.useEffect(() => {
-    fetchIngredients()
-      .then(({ data }) => setData(data))
-      .catch((err) => {
-        setError(err.message);
-      });
-  }, []);
-
-  const addIngredient = (ingredient) => {
-    if (ingredient.type === "bun") {
-      setSelectedIngredients((prevIngredients) => {
-        const ingredientsWithoutBun = prevIngredients.filter(
-          (item) => item.type !== "bun"
-        );
-        return [...ingredientsWithoutBun, ingredient];
-      });
-    } else {
-      setSelectedIngredients((prevIngredients) => [
-        ...prevIngredients,
-        ingredient,
-      ]);
-    }
-  };
-
-  if (error) {
-    return <div>Произошла ошибка: {error}</div>;
-  }
-
+function App() {
   return (
     <div className={styles.app}>
-      <AppHeader />
-      <div className={styles.wrapper}>
-        <IngredientsContext.Provider value={data}>
-          <SelectedIngredientsContext.Provider
-            value={{ selectedIngredients, addIngredient }}>
-            <BurgerIngredients />
-            <BurgerConstructor />
-          </SelectedIngredientsContext.Provider>
-        </IngredientsContext.Provider>
-      </div>
+      <pre style={{
+      	margin: "auto",
+      	fontSize: "1.5rem"
+      }}>
+      	Измените src/components/app/app.jsx и сохраните для обновления.
+      </pre>
     </div>
   );
-};
+}
 
 export default App;
--- src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-const url = 'https://norma.nomoreparties.space/api'
-
-export default url
-
