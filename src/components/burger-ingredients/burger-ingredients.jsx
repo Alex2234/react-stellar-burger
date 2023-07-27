828a0a8106ecd81a1b583907d479a2807992079b
Alex2234 <shyrik223@mail.ru>
2023-07-27 11:55:48 +0300

    fixed month8/step1

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -54,75 +54,75 @@ const BurgerIngredients = () => {
         </Tab>
       </div>
       <div className={`${styles.container} custom-scroll`}>
-          <h3 className="text text_type_main-medium pt-10 pb-6">Булки</h3>
-          <div className={`${styles.ingredients} pl-4 pr-4`}>
-            {buns.map((item) => (
-              <div key={item._id} className={`${styles.ingredient} pb-8`}>
-                <Counter count={1} size="default" extraClass="m-1" />
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="pl-4"
-                  onClick={() => addIngredients(item)}
-                />
-                <div className={`${styles.prices} pt-1 pb-1`}>
-                  <p className="text text_type_digits-default">{item.price}</p>
-                  <CurrencyIcon />
-                </div>
-                <p
-                  className={`${styles.name} text text_type_main-default`}
-                  onClick={() => openModal(item)}>
-                  {item.name}
-                </p>
+        <h3 className="text text_type_main-medium pt-10 pb-6">Булки</h3>
+        <div className={`${styles.ingredients} pl-4 pr-4`}>
+          {buns.map((item) => (
+            <div key={item._id} className={`${styles.ingredient} pb-8`}>
+              <Counter count={1} size="default" extraClass="m-1" />
+              <img
+                src={item.image}
+                alt={item.name}
+                className="pl-4"
+                onClick={() => addIngredients(item)}
+              />
+              <div className={`${styles.prices} pt-1 pb-1`}>
+                <p className="text text_type_digits-default">{item.price}</p>
+                <CurrencyIcon />
               </div>
-            ))}
-          </div>
-          <h3 className="text text_type_main-medium pt-10 pb-6">Соусы</h3>
-          <div className={`${styles.ingredients} pl-4 pr-4`}>
-            {sauces.map((item) => (
-              <div key={item._id} className={`${styles.ingredient} pb-8`}>
-                <Counter count={1} size="default" extraClass="m-1" />
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="pl-4"
-                  onClick={() => addIngredients(item)}
-                />
-                <div className={`${styles.prices} pt-1 pb-1`}>
-                  <p className="text text_type_digits-default">{item.price}</p>
-                  <CurrencyIcon />
-                </div>
-                <p
-                  className={`${styles.name} text text_type_main-default`}
-                  onClick={() => openModal(item)}>
-                  {item.name}
-                </p>
+              <p
+                className={`${styles.name} text text_type_main-default`}
+                onClick={() => openModal(item)}>
+                {item.name}
+              </p>
+            </div>
+          ))}
+        </div>
+        <h3 className="text text_type_main-medium pt-10 pb-6">Соусы</h3>
+        <div className={`${styles.ingredients} pl-4 pr-4`}>
+          {sauces.map((item) => (
+            <div key={item._id} className={`${styles.ingredient} pb-8`}>
+              <Counter count={1} size="default" extraClass="m-1" />
+              <img
+                src={item.image}
+                alt={item.name}
+                className="pl-4"
+                onClick={() => addIngredients(item)}
+              />
+              <div className={`${styles.prices} pt-1 pb-1`}>
+                <p className="text text_type_digits-default">{item.price}</p>
+                <CurrencyIcon />
               </div>
-            ))}
-          </div>
-          <h3 className="text text_type_main-medium pt-10 pb-6">Ингридиенты</h3>
-          <div className={`${styles.ingredients} pl-4 pr-4`}>
-            {mains.map((item) => (
-              <div key={item._id} className={`${styles.ingredient} pb-8`}>
-                <Counter count={1} size="default" extraClass="m-1" />
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="pl-4"
-                  onClick={() => addIngredients(item)}
-                />
-                <div className={`${styles.prices} pt-1 pb-1`}>
-                  <p className="text text_type_digits-default">{item.price}</p>
-                  <CurrencyIcon />
-                </div>
-                <p
-                  className={`${styles.name} text text_type_main-default`}
-                  onClick={() => openModal(item)}>
-                  {item.name}
-                </p>
+              <p
+                className={`${styles.name} text text_type_main-default`}
+                onClick={() => openModal(item)}>
+                {item.name}
+              </p>
+            </div>
+          ))}
+        </div>
+        <h3 className="text text_type_main-medium pt-10 pb-6">Ингридиенты</h3>
+        <div className={`${styles.ingredients} pl-4 pr-4`}>
+          {mains.map((item) => (
+            <div key={item._id} className={`${styles.ingredient} pb-8`}>
+              <Counter count={1} size="default" extraClass="m-1" />
+              <img
+                src={item.image}
+                alt={item.name}
+                className="pl-4"
+                onClick={() => addIngredients(item)}
+              />
+              <div className={`${styles.prices} pt-1 pb-1`}>
+                <p className="text text_type_digits-default">{item.price}</p>
+                <CurrencyIcon />
               </div>
-            ))}
-          </div>
+              <p
+                className={`${styles.name} text text_type_main-default`}
+                onClick={() => openModal(item)}>
+                {item.name}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       <Modal
         title="Детали ингредиента"
